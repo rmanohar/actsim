b2afae8a82c946291a8df6f7c73365b56596af07
Rajit Manohar <rajit.manohar@yale.edu>
2021-11-09 09:37:45 -0500

    script to extract build options from Xyce build

---
+++ grab_xyce.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <path-to-xyce-build-dir>"
+	exit 1
+fi
+
+xyce_build=$1
+
+echo "Xyce build dir: $xyce_build"
+
+if [ ! -d $xyce_build ]
+then
+	echo "Could not find directory $xyce_build"
+	exit 1
+fi
+
+if [ ! -d $xyce_build/src/CMakeFiles/Xyce.dir ]
+then
+	echo "Could not find Xyce sub-directory (src/CMakeFiles/Xyce.dir) in $xyce_build"
+	exit 1
+fi
+
+file=$xyce_build/src/CMakeFiles/Xyce.dir/link.txt
+
+if [ ! -f $file ]
+then
+	echo "Could not find Xyce link.txt file in build directory"
+	exit 1
+fi
+
+linker_stuff=
+compiler_stuff=
+
+found=0
+
+for i in `cat $file`
+do
+	case $found in
+	0) compiler_stuff=$1; found=1;;
+	1) if [ $i = "Xyce" ]
+	   then
+		found=2
+	   fi;;
+	2)  if [ $i = "libxyce.a" ]
+	    then
+		val="-lxyce"
+	    else
+		val=$i
+	    fi;
+	    linker_stuff="${linker_stuff} $val";;
+	esac
+done
+
+echo "Creating xyce.in..."
+
+echo "LIBXYCE=-lxycecinterface $linker_stuff" > xyce.in
